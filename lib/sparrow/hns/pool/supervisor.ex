448e5bb856bfd1ef825cd3bfd796af63da5c4105
Gorkem Duman <gorkem.duman@lydians.com>
2021-06-03 16:39:27 +0300

    Huawei push service support

--- lib/sparrow/hns/pool/supervisor.ex
+++ lib/sparrow/hns/pool/supervisor.ex
@@ -30,7 +30,7 @@ defmodule Sparrow.HNS.V1.Pool.Supervisor do
     children =
       for {{_json, {pool_config, pool_tags}}, index} <-
             Enum.with_index(pool_configs) do
-        id = String.to_atom("Sparrow.Fcm.Pool.ID.#{index}")
+        id = String.to_atom("Sparrow.Hns.Pool.ID.#{index}")
 
         %{
           id: id,
